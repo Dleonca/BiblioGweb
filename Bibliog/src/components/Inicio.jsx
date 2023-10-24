2b1f0c3f282db2de4714e132d1a8afca46b8aa0e
Daniela Leon Cantero <62155571+Dleonca@users.noreply.github.com>
2023-10-23 21:10:42 -0500

    Main en componente inicio

--- Bibliog/src/components/Inicio.jsx
+++ Bibliog/src/components/Inicio.jsx
@@ -6,21 +6,31 @@ import Navbar from "react-bootstrap/Navbar";
 import "../assets/css/general.css";
 const Inicio = () => {
   return (
-    <div  className="img-body">
+    <div className="img-body">
       <Navbar expand="lg" className="bg-whiteOpa">
         <Container fluid>
-          <Navbar.Brand href="#" className="logotipo">BiblioG</Navbar.Brand>
+          <Navbar.Brand href="#" className="logotipo">
+            BiblioG
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="navbarScroll" className="justify-content-end">
-            <Button variant="outline-success" className="me-2">INICIAR SESION</Button>
-            <Button variant="outline-danger" className="me-2">REGISTRATE</Button>
+            <Button variant="outline-success" className="me-2">
+              INICIAR SESION
+            </Button>
+            <Button variant="outline-danger" className="me-2">
+              REGISTRATE
+            </Button>
           </Navbar.Collapse>
         </Container>
       </Navbar>
-      <main>
-        
+      <main className="mainInicio">
+        <h1 className="fw-bold">¿QUE VAS A LEER HOY?</h1>
+        <p className="fs-2">
+          Explora una inmensa cantidad de libros de todo el mundo y enriquece tu
+          vida con nuevas perspectivas.
+        </p>
+        <p className="fs-2">!Prestar libros nunca ha sido tan fácil como con Bibliog!.</p>
       </main>
-
     </div>
   );
 };
