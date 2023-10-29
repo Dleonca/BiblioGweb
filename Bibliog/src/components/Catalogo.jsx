3adda1d079d86c7591953f8a9af8ea01abc2ae34
Daniela Leon Cantero <62155571+Dleonca@users.noreply.github.com>
2023-10-28 21:52:50 -0500

    Actualización

--- Bibliog/src/components/Catalogo.jsx
+++ Bibliog/src/components/Catalogo.jsx
@@ -1,16 +1,22 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 import Card from "react-bootstrap/Card";
-import Libro from "../assets/images/libro.jpg"
+import Libro from "../assets/images/libro.jpg";
 const Catalogo = () => {
   return (
     <div className="catalogoLibros">
-      <div className="filtro"></div>
+      <div className="filtro">
+        <input type="text" id="filtroBusqueda" className="form-control" placeholder="Filtro de Busqueda"/>
+        <button className="btn search" type="submit"> </button>
+      </div>
       <div className="libros">
-        <Card className="tarjeta" >
-          <Card.Title  className="cardCT">Cada historia cuenta</Card.Title>
+        <Card className="tarjeta">
+          <Card.Title className="cardCT">Cada historia cuenta</Card.Title>
           <Card.Img className="cardCI" variant="top" src={Libro} />
-          <Button className="cardCB" variant="primary">Ver más</Button>
+          <Button className="cardCB" variant="primary">
+            Ver más
+          </Button>
         </Card>
       </div>
     </div>
