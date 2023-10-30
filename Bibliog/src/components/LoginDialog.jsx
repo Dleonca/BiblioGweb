614f706e3b6e588102292aa00f1fb565bea7a883
fpatino <=fernandoalfonsopr@gmail.com>
2023-10-29 19:45:26 -0500

    Se agregan dialogos de logis y registro

---
+++ Bibliog/src/components/LoginDialog.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import "../assets/css/general.css";
+
+const LoginDialog = ({ show, handleClose }) => {
+    return (
+      <Modal show={show} onHide={handleClose} centered>
+        <Modal.Header closeButton>
+          <Modal.Title className="title ms-auto">Iniciar sesión</Modal.Title>
+        </Modal.Header>
+        <Modal.Body className="modalBody">
+          <div className="center">
+            <Form.Group controlId="formBasicEmail" style={{ width: '100%' }}>
+              <Form.Label>Correo electrónico</Form.Label>
+              <Form.Control type="email" placeholder="Ingrese su correo electrónico" className="input" style={{ width: '100%' }} />
+            </Form.Group>
+  
+            <Form.Group controlId="formBasicPassword" style={{ width: '100%' }}>
+              <Form.Label>Contraseña</Form.Label>
+              <Form.Control type="password" placeholder="Contraseña" className="input" style={{ width: '100%' }} />
+            </Form.Group>
+  
+            <p className="forgotPassword">
+              <a href="#">¿Olvidaste tu contraseña?</a>
+            </p>
+  
+            <Button variant="primary" type="submit" className="button" style={{ width: '100%' }}>
+              Iniciar sesión
+            </Button>
+          </div>
+        </Modal.Body>
+      </Modal>
+    );
+  };
+  
+  export default LoginDialog;
